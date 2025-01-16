4c690b9c270d4eaa2d6edf9e926384b1314b9190
Thomas Peterson <thomasep@pm.me>
2025-01-16 16:01:13 +1100

    fixing dual config issues

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,18 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts',
     '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/*.d.ts'
   ],
   verbose: true,
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
